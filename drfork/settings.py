322c9be194fe2af8543a8c2b8ad9ec6ab9c8a842
tcitry <tcitry@gmail.com>
2017-04-18 14:56:37 +0800

    migrate apps

--- drfork/settings.py
+++ drfork/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'account',
-    'topic',
+    'apps.account',
+    'apps.topic',
 ]
 
 MIDDLEWARE = [
--- drfork/urls.py
+++ drfork/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('topic.urls'))
+    url(r'^', include('apps.topic.urls'))
 ]
